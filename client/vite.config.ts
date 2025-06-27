80596e5792753751800d2b4db0d92ebed73cda89
Omoroie <justsoul12@naver.com>
2025-06-27 19:36:56 +0900

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: path.resolve(__dirname, "..", "dist"),
+    outDir: path.resolve(__dirname, "..", "dist/public"),
     emptyOutDir: true,
   },
   server: {
